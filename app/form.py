a0865c58bb825f3f56517db096fa5a065bd38532
Matthew Patterson <matthew.patterson@mymona.uwi.edu>
2018-02-19 21:37:38 +0000

    House cleaning

--- app/form.py
+++ app/form.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, flash, session, redirect, url_for
 from wtforms import TextAreaField
 from flask_wtf import FlaskForm
-from flask_wtf.recaptcha import RecaptchaField
-
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email
@@ -16,11 +14,11 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 class MyForm(FlaskForm):
-    firstname = StringField('First Name', validators=[DataRequired()])
-    lastname = StringField('Last Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    subject = StringField('Subject', validators=[DataRequired()])
-    message= TextAreaField("Message", validators=[DataRequired()])
+    firstname = StringField('First Name', validators=[DataRequired("Please enter your firstname")])
+    lastname = StringField('Last Name', validators=[DataRequired("Please enter your lastname")])
+    email = StringField('Email', validators=[DataRequired("Please enter your e-mail address"), Email()])
+    subject = StringField('Subject', validators=[DataRequired("Please enter the subject of your message")])
+    message= TextAreaField("Message", validators=[DataRequired("Please enter the message you would like to send")])
 
 if __name__ == "__main__":
     app.run()
